915a03c5108ac74be965ff976e127463274ef531
qcgm1978 <zhanghl@strongunion.com.cn>
2017-03-02 10:15:28 +0800

    add source map

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -5,7 +5,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-  devtool: 'eval-source-map',
+  devtool: 'inline-source-map',
   entry: { 
     // 'react': 'react',
     // 'react-dom': 'react-dom',
